27400f11439ea7a3048b57c32c86aa4e2b67e1ac
Henmasta <39940952+Henmasta@users.noreply.github.com>
2020-11-02 18:32:09 +0300

    Update svitgen.py

--- svitgen.py
+++ svitgen.py
@@ -1,9 +1,12 @@
-# Svitgen beta v0.3 
+# Svitgen beta v0.83
 import time
 import os
 import sys
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
 # Лого
-print("=============================")
+print("=============================================")
 print("""
 
   ______     _____ ____   ____ _____ _   _  
@@ -13,7 +16,7 @@ print("""
  |____/  \_/  |___|____/ \____|_____|_| \_| 
                                             
 """)
-print("=============================")
+print("=============================================")
 # Действия
 print("""
 1. Генерировать""")
@@ -22,10 +25,10 @@ print("3. Выйти")
 a = input("Введите действие: ")	
 if a == "3":
 	sys.exit()
-if a == "2":
+elif a == "2":
 	print("Это приложение SVIDGEN. Оно специализируется на генерации свидание по таким параметра как время и бюджет.")
 	sys.exit()
-if a == "1" or "Генерировать" or "генерировать":
+elif a == "1" or "Генерировать" or "генерировать":
 	print("1. Утро (до 12 часов)")
 	print("2. День (с 13 до 18)")
 	print("3. Вечер (с 19 до 22)")
@@ -39,29 +42,59 @@ if vremya == "1" or vremya == "Утро" or vremya == "утро":
 		time.sleep(0.5)
 		print("Можно: \nКупить мороженное или булки \nКупить напиток \nПогулять по парку \nПокататься на велосипедах")
 	
-	if but == "150":
-		print('debil')
+	elif but == "150":
+		time.sleep(0.5)
+		print('1/150')
+
+	elif but == "200":
+		time.sleep(0.5)
+		print("1/200")
+		
+	elif but == '250':
+		time.sleep(0.5)
+		print('1/250')
+
+	elif but == '300':
+		time.sleep(0.5)
+		print()
+
+	elif but == '350':
+		time.sleep(0.5)
+		print()
+
+	elif but == '400':
+		time.sleep(0.5)
+		print('')
+
+	elif but == '450':
+		time.sleep(0.5)
+		print('')
+
+	elif but == '500':
+		time.sleep(0.5)
+		print('')
+
 # Бюджет если день
 if vremya == "2" or vremya == "День" or vremya == "день":
 	if but == '100':
 		time.sleep(0.5)
-		print("")
+		print("2/100")
 
 	elif but == "150":
 		time.sleep(0.5)
-		print("lll")
+		print("2/150")
 
 	elif but == '200':
 		time.sleep(0.5)
-		print()
+		print("2/200")
 
 	elif but == '250':
 		time.sleep(0.5)
-		print()
+		print("2/250")
 
 	elif but == '300':
 		time.sleep(0.5)
-		print()
+		print("2/300")
 
 	elif but == '350':
 		time.sleep(0.5)
@@ -78,11 +111,12 @@ if vremya == "2" or vremya == "День" or vremya == "день":
 	elif but == '500':
 		time.sleep(0.5)
 		print()
+
 # Бютжет если вечер
 if vremya == "3" or vremya == "Вечер" or vremya == "вечер":
 	if but == '100':
 		time.sleep(0.5)
-		print("")
+		print("3/100")
 
 	elif but == "150":
 		time.sleep(0.5)
@@ -115,6 +149,7 @@ if vremya == "3" or vremya == "Вечер" or vremya == "вечер":
 	elif but == '500':
 		time.sleep(0.5)
 		print()
+
 # Бютжет если ночь
 if vremya == "4" or vremya == "Ночь" or vremya == "ночь":
 	if but == '100':
